28ee90d83bdc21def73daa741fe4d05613527fb9
JavelinSx <zuev499@gmail.com>
2022-09-10 22:18:17 +0300

    progress css

--- src/components/Pacients.js
+++ src/components/Pacients.js
@@ -4,37 +4,90 @@ function Pacients(){
         <div className='pacients'>
             <div className='pacients__header'>База данных пациентов</div>
             <div className='pacients__work-section'>
-                <div className="form-input-container pacients__form-search">
+                <div className="pacients__search-container">
+                    <input type='search' id='search' className="form-input pacients__input-search" requaried>
 
-                        <input type='search' id='search' className="form-input pacients__input-search" requaried>
-
-                        </input>
-                        <label for='search' className="form-label pacients__label">
-                            Поиск
-                        </label>
-                        <button className='pacients__button-search button-animation'>Искать</button>
+                    </input>
+                    <label for='search' className="form-label pacients__label">
+                        Поиск
+                    </label>
+                    <button className='pacients__button-search button-animation'>Искать</button>
                 </div>
+
 
                 <button className='pacients__button-add-pacient button-animation'>Добавить&nbsp;пациента</button>
                 <button className='pacients__button-update button-animation'>Обновить&nbsp;список&nbsp;пациентов</button>
+
             </div>
             <div className='pacients__flow'>
-                <h2 className='pacients__header-title-list'>
-                        Список пациентов
-                </h2>
-                <div className='pacients__list-container'>
-                    <ul className='pacients-list'>
+                <div className="pacients__flow-container-list">
+                    <h2 className='pacients__header-title-list'>
+                            Список пациентов
+                    </h2>
 
-                    </ul>
-                </div>
-                <h2 className='pacients__header-title-info'>
-                        Информация/Результаты анализов
-                </h2>
-                <div className='pacients__info'>
                     <ul className='pacients__list'>
-
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
+                        <li className="pacients__list-item button-animation">
+                            Зуев Никита Андреевич
+                        </li>
                     </ul>
                 </div>
+                
+                <div className="pacients__flow-container-info">
+                    <h2 className='pacients__header-title-info'>
+                            Информация/Результаты анализов
+                    </h2>
+                    <div className='pacients__info'>
+                        <ul className='pacients__list-info'>
+
+                        </ul>
+                    </div>
+                </div>
+
             </div>
 
         </div>
